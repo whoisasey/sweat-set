2110e7ab328fb0dfda64637e5f01c66ffcb811b9
atina <atinachang@gmail.com>
2025-03-01 17:24:33 -0500

    add route

---
+++ app/api/singleExercise/add/route.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from "next/server";
+import SingleExercise from "@/app/models/SingleExercise";
+import connect from "@/app/utils/db";
+
+export const POST = async (req: Request) => {
+	try {
+		await connect();
+		console.log("add exercise...");
+		const body = await req.json();
+
+		// Validate and transform weights
+		const weights = Object.keys(body)
+			.filter((key) => key.startsWith("weight-")) // Filter keys that start with "weight-"
+			.map((key) => Number(body[key])) // Convert each value to a number
+			.filter((num) => !isNaN(num)); // Filter out any NaN values
+
+		// Constrcut exercise object
+		const newExercise = new SingleExercise({
+			exercise: body.exercise,
+			exerciseId: body.exerciseId,
+			weights,
+			sets: Number(body.sets) || 0,
+			reps: Number(body.reps) || 0,
+			distance: Number(body.distance) || 0,
+			userId: body.userId,
+			date: new Date(body.date),
+		});
+
+		// Save to DB
+		const savedExercise = await newExercise.save();
+		console.log("Exercise saved 💦");
+
+		return NextResponse.json(savedExercise, { status: 201 });
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			console.error("Error adding message:", error);
+			const statusCode = error.name === "ValidationError" ? 400 : 500;
+			return NextResponse.json(
+				{ error: error.message || "An unexpected error occurred" },
+				{ status: statusCode },
+			);
+		}
+
+		// If the error is not an instance of Error, handle accordingly
+		console.error("Unknown error occurred:", error);
+		return NextResponse.json(
+			{ error: "An unexpected error occurred" },
+			{ status: 500 },
+		);
+	}
+};
