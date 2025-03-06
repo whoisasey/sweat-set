89f9c2d07b27fd69696d9633e5773ffd585b0cbd
atina <56001902+whoisasey@users.noreply.github.com>
2025-03-05 21:47:37 -0500

    basic navigation (#34)

---
+++ app/components/Nav.tsx
@@ -0,0 +1,27 @@
+import { Box, Link } from "@mui/material";
+
+import React from "react";
+
+const Nav = () => {
+	return (
+		<Box sx={{ display: "flex", justifyContent: "center" }}>
+			<Box
+				sx={{
+					maxWidth: "fit-content",
+					display: "flex",
+					alignItems: "center",
+					textAlign: "center",
+					p: 3,
+				}}>
+				<Link href="/" sx={{ minWidth: 100 }}>
+					Home
+				</Link>
+				<Link href="/progress" sx={{ minWidth: 100 }}>
+					Progress
+				</Link>
+			</Box>
+		</Box>
+	);
+};
+
+export default Nav;
