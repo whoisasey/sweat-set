b01377d4d4f559d92a686481790a266b3fc59753
atina <atinachang@gmail.com>
2025-04-11 19:49:01 -0400

    homepage redirect authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
-import ExercisePlan from "./components/ExercisePlan";
+"use client";
 
-export default function Home() {
-	return (
-		<>
-			<ExercisePlan />
-		</>
-	);
-}
+import ExercisePlan from "@/app/components/ExercisePlan";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+const Home = () => {
+	const router = useRouter();
+	const { status } = useSession();
+
+	useEffect(() => {
+		if (status === "unauthenticated") {
+			router.push("/login");
+		}
+	}, [status, router]);
+
+	if (status === "unauthenticated") return null; // Prevent premature render
+
+	return <ExercisePlan />;
+};
+
+export default Home;
