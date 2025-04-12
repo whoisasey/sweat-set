af8ba25323159dbab6bb94085fc9c61b9cdde3e7
atina <56001902+whoisasey@users.noreply.github.com>
2025-04-11 20:00:37 -0400

    62 user auth (#63)
    * user redirect, nav if user not authenticated

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,10 +1,32 @@
 "use client";
 
 import { Box, Link } from "@mui/material";
+import { signIn, signOut } from "next-auth/react";
 
 import React from "react";
+import { useSession } from "next-auth/react";
 
 const Nav = () => {
+	const { status } = useSession();
+
+	if (status === "unauthenticated") {
+		return (
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					textAlign: "center",
+					justifyContent: "space-around",
+					py: 3,
+				}}>
+				<Link href="/">Home</Link>
+				<Link component={"button"} onClick={() => signIn()}>
+					Login
+				</Link>
+			</Box>
+		);
+	}
+
 	return (
 		<Box
 			sx={{
@@ -16,7 +38,13 @@ const Nav = () => {
 			}}>
 			<Link href="/">Home</Link>
 			<Link href="/progress">Progress</Link>
-			<Link href="/login">Login</Link>
+			<Link
+				component={"button"}
+				onClick={() => {
+					signOut();
+				}}>
+				Log Out
+			</Link>
 			<Link href="/dashboard">Dashboard</Link>
 		</Box>
 	);
