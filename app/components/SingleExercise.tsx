68d0b7eb9b17bb12374dd7c43f7007d87eaeb8bb
atina <56001902+whoisasey@users.noreply.github.com>
2025-04-19 16:42:21 -0400

    27 optimize handlesubmit (#66)
    * optimize handlesubmit

--- app/components/SingleExercise.tsx
+++ app/components/SingleExercise.tsx
@@ -2,6 +2,7 @@
 
 import { Box, Button, IconButton, InputLabel } from "@mui/material";
 import React, { FormEvent, useEffect, useState } from "react";
+import { addNewExercise, checkIfExerciseExists, submitExerciseData } from "@/app/utils/helpers-fe";
 
 // import { CSS } from "@dnd-kit/utilities";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -15,306 +16,244 @@ import { useSession } from "next-auth/react";
 // import { useSortable } from "@dnd-kit/sortable";
 
 export const ExerciseForm = ({ onRemove, name, sets, reps }: ExerciseProps) => {
-	const [selectedExercise, setSelectedExercise] = useState<string>("");
-	const [updatedSets, setUpdatedSets] = useState<number>(0); // Initial sets value
-	const [updatedReps, setUpdatedReps] = useState<number[]>([]); //start with an empty array
-	const [date, setDate] = useState<Date>(new Date());
-	const [weights, setWeights] = useState<number[]>([]); // Start with an empty array
-	const [newExercise, setNewExercise] = useState<string>("");
-	const [userId, setUserId] = useState<string | undefined>("");
-	const [isNewExercise, setIsNewExercise] = useState(false);
-	const [allExercises, setAllExercises] = useState<Exercise[]>([]);
-	const [successMsg, setSuccessMsg] = useState<string>("");
-	const session = useSession();
+  const [selectedExercise, setSelectedExercise] = useState<string>("");
+  const [updatedSets, setUpdatedSets] = useState<number>(0); // Initial sets value
+  const [updatedReps, setUpdatedReps] = useState<number[]>([]); //start with an empty array
+  const [date, setDate] = useState<Date>(new Date());
+  const [weights, setWeights] = useState<number[]>([]); // Start with an empty array
+  const [newExercise, setNewExercise] = useState<string>("");
+  const [userId, setUserId] = useState<string | undefined>("");
+  const [isNewExercise, setIsNewExercise] = useState(false);
+  const [allExercises, setAllExercises] = useState<Exercise[]>([]);
+  const [successMsg, setSuccessMsg] = useState<string>("");
+  const session = useSession();
 
-	useEffect(() => {
-		const fetchExercises = async () => {
-			try {
-				const response = await fetch("/api/exercise/get");
-				if (!response.ok) {
-					throw new Error("Failed to fetch exercises");
-				}
+  useEffect(() => {
+    const fetchExercises = async () => {
+      try {
+        const response = await fetch("/api/exercise/get");
+        if (!response.ok) {
+          throw new Error("Failed to fetch exercises");
+        }
 
-				const data: Exercise[] = await response.json();
-				setAllExercises(data);
-			} catch (err) {
-				if (err instanceof Error) {
-					console.log(err.message);
-				} else {
-					throw new Error("An unexpected Error occurred");
-				}
-			}
-		};
+        const data: Exercise[] = await response.json();
+        setAllExercises(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          console.log(err.message);
+        } else {
+          throw new Error("An unexpected Error occurred");
+        }
+      }
+    };
 
-		fetchExercises();
-	}, []);
+    fetchExercises();
+  }, []);
 
-	useEffect(() => {
-		setUpdatedReps(reps as number[]);
-	}, [reps]);
+  useEffect(() => {
+    setUpdatedReps(reps as number[]);
+  }, [reps]);
 
-	useEffect(() => {
-		if (sets) setUpdatedSets(sets);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [sets]);
+  useEffect(() => {
+    if (sets) setUpdatedSets(sets);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [sets]);
 
-	// Update weights when sets change
-	useEffect(() => {
-		// If the number of sets changes, update the weights array to match the new number of sets
-		setWeights(Array.from({ length: updatedSets }, () => 0) as number[]);
-	}, [updatedSets]);
+  // Update weights when sets change
+  useEffect(() => {
+    // If the number of sets changes, update the weights array to match the new number of sets
+    setWeights(Array.from({ length: updatedSets }, () => 0) as number[]);
+  }, [updatedSets]);
 
-	useEffect(() => {
-		setUserId(session?.data?.user?.id);
-	}, [session]);
+  useEffect(() => {
+    setUserId(session?.data?.user?.id);
+  }, [session]);
 
-	useEffect(() => {
-		if (name) handleChange(name);
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [name]);
+  useEffect(() => {
+    if (name) handleChange(name);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [name]);
 
-	useEffect(() => {
-		if (reps) handleInputChange(0, reps, "rep");
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [reps]);
+  useEffect(() => {
+    if (reps) handleInputChange(0, reps, "rep");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [reps]);
 
-	// TODO: add error handling if selectedExercise does not have a value
-	// TODO: based on day of the week of the 2-week split, show different exercises
-	const handleChange = (e: React.ChangeEvent<HTMLSelectElement> | string) => {
-		const newValue = typeof e === "string" ? e : e.target.value;
+  // TODO: add error handling if selectedExercise does not have a value
+  // TODO: based on day of the week of the 2-week split, show different exercises
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement> | string) => {
+    const newValue = typeof e === "string" ? e : e.target.value;
 
-		// Only update if the value has changed
-		if (newValue !== selectedExercise) {
-			setSelectedExercise(newValue);
-		}
+    // Only update if the value has changed
+    if (newValue !== selectedExercise) {
+      setSelectedExercise(newValue);
+    }
 
-		if (newValue === "Not Listed") {
-			setIsNewExercise(true);
-		}
-	};
+    if (newValue === "Not Listed") {
+      setIsNewExercise(true);
+    }
+  };
 
-	// Handles  input changes
-	const handleInputChange = (
-		index: number,
-		value: string | number[],
-		field?: string,
-	) => {
-		// Convert input value to a number, default to 1 if invalid (except for weights)
-		const numValue = Number(value) || 1;
+  // Handles  input changes
+  const handleInputChange = (index: number, value: string | number[], field?: string) => {
+    // Convert input value to a number, default to 1 if invalid (except for weights)
+    const numValue = Number(value) || 1;
 
-		const stringVal = typeof value === "string" ? value : "";
-		switch (field) {
-			case "sets": {
-				// Ensure sets is always at least 1
-				const newSets = Math.max(numValue, 1);
-				setUpdatedSets(newSets);
-				// console.log(updatedSets);
+    const stringVal = typeof value === "string" ? value : "";
+    switch (field) {
+      case "sets": {
+        // Ensure sets is always at least 1
+        const newSets = Math.max(numValue, 1);
+        setUpdatedSets(newSets);
+        // console.log(updatedSets);
 
-				// Adjust the weights array length based on the new number of sets
-				setWeights(
-					(prevWeights) =>
-						newSets > prevWeights.length
-							? [...prevWeights, ...Array(newSets - prevWeights.length).fill(0)] // Expand with zeros
-							: prevWeights.slice(0, newSets), // Trim excess weights
-				);
-				return;
-			}
+        // Adjust the weights array length based on the new number of sets
+        setWeights(
+          (prevWeights) =>
+            newSets > prevWeights.length
+              ? [...prevWeights, ...Array(newSets - prevWeights.length).fill(0)] // Expand with zeros
+              : prevWeights.slice(0, newSets) // Trim excess weights
+        );
+        return;
+      }
 
-			case "date":
-				// Convert the input value to a Date object
-				setDate(new Date(stringVal));
-				return;
+      case "date":
+        // Convert the input value to a Date object
+        setDate(new Date(stringVal));
+        return;
 
-			case "newExercise":
-				setNewExercise(capitalizeWords(stringVal));
-				setSelectedExercise(stringVal);
-				return;
+      case "newExercise":
+        setNewExercise(capitalizeWords(stringVal));
+        setSelectedExercise(stringVal);
+        return;
 
-			default:
-				// Handle weight input fields dynamically
-				if (field?.startsWith("weight")) {
-					setWeights((prevWeights) => {
-						const newWeights = [...prevWeights];
+      default:
+        // Handle weight input fields dynamically
+        if (field?.startsWith("weight")) {
+          setWeights((prevWeights) => {
+            const newWeights = [...prevWeights];
 
-						newWeights[index] = Number(value) || 0; // Ensure weight defaults to 0 if invalid
+            newWeights[index] = Number(value) || 0; // Ensure weight defaults to 0 if invalid
 
-						return newWeights;
-					});
-				}
-				if (field?.startsWith("rep")) {
-					setUpdatedReps((prevReps) => {
-						const newReps = [...prevReps];
-						newReps[index] = Number(value) || newReps[index];
-						return newReps;
-					});
-				}
-		}
-	};
+            return newWeights;
+          });
+        }
+        if (field?.startsWith("rep")) {
+          setUpdatedReps((prevReps) => {
+            const newReps = [...prevReps];
+            newReps[index] = Number(value) || newReps[index];
+            return newReps;
+          });
+        }
+    }
+  };
 
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-		const formData = new FormData(e.target as HTMLFormElement);
-		const data: Record<string, unknown> = {};
+    const formData = new FormData(e.target as HTMLFormElement);
+    const data: Record<string, unknown> = Object.fromEntries(formData.entries());
 
-		formData.forEach((value, key) => {
-			data[key] = value;
-		});
+    Object.assign(data, {
+      sets,
+      reps: updatedReps,
+      exerciseId: selectedExercise,
+      weights,
+      distance: selectedExercise === "running" ? Number(data.distance) || 0 : 0,
+      userId,
+      date,
+      exercise: selectedExercise || newExercise,
+    });
 
-		// Add form data
-		data.sets = sets;
-		data.reps = updatedReps;
-		data.exerciseId = selectedExercise;
-		data.weights = weights;
-		data.distance =
-			selectedExercise === "running" ? Number(data.distance) || 0 : 0;
-		data.userId = userId; // Replace with actual user ID logic
-		data.date = date;
-		data.exercise = selectedExercise ?? newExercise;
+    try {
+      if (newExercise) {
+        const exerciseExists = await checkIfExerciseExists(newExercise);
+        if (!exerciseExists) {
+          await addNewExercise(newExercise);
+        } else {
+          console.log("Exercise already exists!");
+        }
+      }
 
-		// only run this if newExercise has a value
-		if (newExercise !== "") {
-			try {
-				const checkResponse = await fetch(
-					`/api/exercise/check?name=${newExercise}`,
-					{
-						method: "GET",
-						headers: {
-							"Content-Type": "application/json",
-						},
-					},
-				);
+      await submitExerciseData(data);
+      setSuccessMsg("Added Exercise Set 💪🏻");
+    } catch (error) {
+      console.error("Error submitting exercise:", error);
+    }
+  };
 
-				if (!checkResponse.ok) {
-					throw new Error("Failed to check if exercise exists");
-				}
+  return (
+    <form onSubmit={handleSubmit}>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <InputLabel htmlFor="exercise">Exercise</InputLabel>
+        <Box>
+          <select
+            name="exercise"
+            id="exercise"
+            value={selectedExercise}
+            style={{
+              width: "100%",
+              padding: "8px",
+              borderRadius: "4px",
+              border: "1px solid #ccc",
+              fontSize: "16px",
+            }}
+            onChange={(e) => handleChange(e as React.ChangeEvent<HTMLSelectElement>)}
+          >
+            {(!allExercises || allExercises.length === 0) && <option value=""></option>}
+            {allExercises
+              .sort((a, b) => a.exerciseName.localeCompare(b.exerciseName))
+              .map((exercise) => (
+                <option key={exercise._id} value={exercise.exerciseName}>
+                  {exercise.exerciseName}
+                </option>
+              ))}
+            {allExercises && allExercises.length >= 0 && <option value="Not Listed">Exercise Not Listed ➕</option>}
+          </select>
+        </Box>
+        {isNewExercise ? (
+          <Box sx={{ display: "flex", flexDirection: "column" }}>
+            <label htmlFor="newExercise">Input New Exercise</label>
+            <input
+              type="text"
+              name="newExercise"
+              id="newExercise"
+              value={newExercise}
+              onChange={(e) => handleInputChange(0, (e.target as HTMLInputElement).value, "newExercise")}
+              style={{
+                padding: "8px",
+                borderRadius: "4px",
+                border: "1px solid #ccc",
+                fontSize: "16px",
+              }}
+            />
+          </Box>
+        ) : null}
 
-				const checkResult = await checkResponse.json();
-
-				if (!checkResult.exists) {
-					const addResponse = await fetch("/api/exercise/add", {
-						method: "POST",
-						headers: { "Content-Type": "application/json" },
-						body: JSON.stringify({ exerciseName: newExercise }),
-					});
-
-					if (!addResponse.ok) {
-						throw new Error("Failed to add new exercise");
-					}
-
-					// const addResult = await addResponse.json();
-				} else {
-					console.log("Exercise already exists!");
-				}
-			} catch (error) {
-				console.error("Error submitting exercise:", error);
-			}
-		}
-
-		try {
-			const response = await fetch("/api/exerciseSet/add", {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify(data),
-			});
-
-			if (!response.ok) {
-				throw new Error("Failed to submit exercise data.");
-			}
-			setSuccessMsg("Added Exercise Set 💪🏻");
-			// const result = await response.json();
-			// console.log("Success :", result);
-			// TODO: Handle success
-		} catch (error) {
-			console.error("Error submitting exercise:", error);
-		}
-	};
-	// console.log(updatedSets);
-
-	return (
-		<form onSubmit={handleSubmit}>
-			<Box
-				sx={{
-					display: "flex",
-					flexDirection: "column",
-				}}>
-				<InputLabel htmlFor="exercise">Exercise</InputLabel>
-				<Box>
-					<select
-						name="exercise"
-						id="exercise"
-						value={selectedExercise}
-						style={{
-							width: "100%",
-							padding: "8px",
-							borderRadius: "4px",
-							border: "1px solid #ccc",
-							fontSize: "16px",
-						}}
-						onChange={(e) =>
-							handleChange(e as React.ChangeEvent<HTMLSelectElement>)
-						}>
-						{(!allExercises || allExercises.length === 0) && (
-							<option value=""></option>
-						)}
-						{allExercises
-							.sort((a, b) => a.exerciseName.localeCompare(b.exerciseName))
-							.map((exercise) => (
-								<option key={exercise._id} value={exercise.exerciseName}>
-									{exercise.exerciseName}
-								</option>
-							))}
-						{allExercises && allExercises.length >= 0 && (
-							<option value="Not Listed">Exercise Not Listed ➕</option>
-						)}
-					</select>
-				</Box>
-				{isNewExercise ? (
-					<Box sx={{ display: "flex", flexDirection: "column" }}>
-						<label htmlFor="newExercise">Input New Exercise</label>
-						<input
-							type="text"
-							name="newExercise"
-							id="newExercise"
-							value={newExercise}
-							onChange={(e) =>
-								handleInputChange(
-									0,
-									(e.target as HTMLInputElement).value,
-									"newExercise",
-								)
-							}
-							style={{
-								padding: "8px",
-								borderRadius: "4px",
-								border: "1px solid #ccc",
-								fontSize: "16px",
-							}}
-						/>
-					</Box>
-				) : null}
-
-				<WeightInput
-					selectedExercise={selectedExercise}
-					sets={sets}
-					updatedSets={updatedSets}
-					updatedReps={updatedReps}
-					setUpdatedReps={setUpdatedReps}
-					weights={weights}
-					handleInputChange={handleInputChange}
-					setUpdatedSets={setUpdatedSets}
-					date={date}
-				/>
-			</Box>
-			<IconButton color="error" onClick={onRemove}>
-				<DeleteIcon />
-			</IconButton>
-			<p>{successMsg}</p>
-			<Button type="submit" variant="contained" color="primary">
-				Submit
-			</Button>
-		</form>
-	);
+        <WeightInput
+          selectedExercise={selectedExercise}
+          sets={sets}
+          updatedSets={updatedSets}
+          updatedReps={updatedReps}
+          setUpdatedReps={setUpdatedReps}
+          weights={weights}
+          handleInputChange={handleInputChange}
+          setUpdatedSets={setUpdatedSets}
+          date={date}
+        />
+      </Box>
+      <IconButton color="error" onClick={onRemove}>
+        <DeleteIcon />
+      </IconButton>
+      <p>{successMsg}</p>
+      <Button type="submit" variant="contained" color="primary">
+        Submit
+      </Button>
+    </form>
+  );
 };
--- app/data/workoutPlan.js
+++ app/data/workoutPlan.js
@@ -1,127 +1,127 @@
 export const workoutPlan = [
-	{
-		week: 1,
-		focus: "Endurance & Strength Stability",
-		days: [
-			{
-				weekday: "Sunday",
-				date: "Sunday, April 13, 2025",
-				type: "Full Body Strength Training",
-				exercises: [
-					{ name: "Romanian Deadlift", reps: [10, 10, 10], sets: 3 },
-					{ name: "Incline Dumbbell Press", reps: [10, 10, 10], sets: 3 },
-					{ name: "Lat Pulldown", reps: [10, 10, 10], sets: 3 },
-					{ name: "Step-Through Lunges", reps: [10, 10, 10], sets: 3 },
-					{ name: "Core: Hanging Knee Raises", reps: [10, 10, 10], sets: 3 },
-				],
-			},
-			{
-				weekday: "Monday",
-				date: "Monday, April 14, 2025",
-				type: "Recovery Day",
-			},
-			{
-				weekday: "Tuesday",
-				date: "Tuesday, April 15, 2025",
-				type: "Short Strength (Unilateral Lower Body, 30-40 min)",
-				exercises: [
-					{ name: "Bulgarian Split Squat", reps: [10, 10, 10], sets: 3 },
-					{ name: "Single-Leg Romanian Deadlift", reps: [10, 10, 10], sets: 3 },
-					{ name: "Step Up", reps: [10, 10, 10], sets: 3 },
-					{ name: "Core: Hanging Leg Raises", reps: [10, 10, 10], sets: 3 },
-				],
-			},
-			{
-				weekday: "Wednesday",
-				date: "Wednesday, April 16, 2025",
-				type: "Running (Endurance Focus, 25-30 min)",
-				workout: "Run/Walk Intervals: 4 min run / 1 min walk (repseat 5x)",
-			},
-			{
-				weekday: "Thursday",
-				date: "Thursday, April 17, 2025",
-				type: "Recovery Day",
-			},
-			{
-				weekday: "Friday",
-				date: "Friday, April 18, 2025",
-				type: "Short Upper Body Strength (30-40 min)",
-				exercises: [
-					{
-						name: "Single-Arm Dumbbell Overhead Press",
-						reps: [10, 10, 10],
-						sets: 3,
-					},
-					{ name: "Dumbbell Row", reps: [10, 10, 10], sets: 3 },
-					{ name: "Core: Pallof Press", reps: [10, 10, 10], sets: 3 },
-				],
-			},
-			{
-				weekday: "Saturday",
-				date: "Saturday, April 19, 2025",
-				type: "Hot Yoga (Mobility & Recovery)",
-			},
-		],
-	},
-	{
-		week: 2,
-		focus: "Progression & Speed Focus",
-		days: [
-			{
-				weekday: "Sunday",
-				date: "Sunday, April 20, 2025",
-				type: "Hot Yoga (Mobility & Recovery)",
-			},
-			{
-				weekday: "Monday",
-				date: "Monday, April 21, 2025",
-				type: "Short Strength (Unilateral Lower Body, 30-40 min)",
-				exercises: [
-					{ name: "Lateral Lunges", reps: [10, 10, 10], sets: 3 },
-					{ name: "Single Leg Pistol W Box ", reps: [10, 10, 10], sets: 3 },
-					{ name: "Hip Thrust", reps: [10, 10, 10], sets: 3 },
-					{ name: "Core: Reverse Crunches", reps: [10, 10, 10], sets: 3 },
-				],
-			},
-			{
-				weekday: "Tuesday",
-				date: "Tuesday, April 22, 2025",
-				type: "Running (Speed or Tempo Focus, 20-30 min)",
-				workout: "30-sec sprint + 90-sec walk (repseat 6-8x)",
-			},
-			{
-				weekday: "Wednesday",
-				date: "Wednesday, April 23, 2025",
-				type: "Recovery Day",
-			},
-			{
-				weekday: "Thursday",
-				date: "Thursday, April 24, 2025",
-				type: "Short Upper Body Strength (30-40 min)",
-				exercises: [
-					{
-						name: "Single-Arm Dumbbell Bench Press",
-						reps: [10, 10, 10],
-						sets: 3,
-					},
-					{ name: "Dumbbell Snatch", reps: [10, 10, 10], sets: 3 },
-					{
-						name: "Core: Side Plank with Hip Dips",
-						reps: [10, 10, 10],
-						sets: 3,
-					},
-				],
-			},
-			{
-				weekday: "Friday",
-				date: "Friday, April 25, 2025",
-				type: "Jungle TO",
-			},
-			{
-				weekday: "Saturday",
-				date: "Saturday, April 26, 2025",
-				type: "Recovery Day",
-			},
-		],
-	},
+  {
+    week: 1,
+    focus: "Endurance & Strength Stability",
+    days: [
+      {
+        weekday: "Sunday",
+        date: "Sunday, April 13, 2025",
+        type: "Full Body Strength Training",
+        exercises: [
+          { name: "Romanian Deadlift", reps: [10, 10, 10], sets: 3 },
+          { name: "Incline Dumbbell Press", reps: [10, 10, 10], sets: 3 },
+          { name: "Lat Pulldown", reps: [10, 10, 10], sets: 3 },
+          { name: "Step-Through Lunges", reps: [10, 10, 10], sets: 3 },
+          { name: "Core: Hanging Knee Raises", reps: [10, 10, 10], sets: 3 },
+        ],
+      },
+      {
+        weekday: "Monday",
+        date: "Monday, April 14, 2025",
+        type: "Recovery Day",
+      },
+      {
+        weekday: "Tuesday",
+        date: "Tuesday, April 15, 2025",
+        type: "Short Strength (Unilateral Lower Body, 30-40 min)",
+        exercises: [
+          { name: "Bulgarian Split Squat", reps: [10, 10, 10], sets: 3 },
+          { name: "Single-Leg Romanian Deadlift", reps: [10, 10, 10], sets: 3 },
+          { name: "Step Up", reps: [10, 10, 10], sets: 3 },
+          { name: "Core: Hanging Leg Raises", reps: [10, 10, 10], sets: 3 },
+        ],
+      },
+      {
+        weekday: "Wednesday",
+        date: "Wednesday, April 16, 2025",
+        type: "Running (Endurance Focus, 25-30 min)",
+        workout: "Run/Walk Intervals: 4 min run / 1 min walk (repseat 5x)",
+      },
+      {
+        weekday: "Thursday",
+        date: "Thursday, April 17, 2025",
+        type: "Recovery Day",
+      },
+      {
+        weekday: "Friday",
+        date: "Friday, April 18, 2025",
+        type: "Short Upper Body Strength (30-40 min)",
+        exercises: [
+          {
+            name: "Single-Arm Dumbbell Overhead Press",
+            reps: [10, 10, 10],
+            sets: 3,
+          },
+          { name: "Dumbbell Row", reps: [10, 10, 10], sets: 3 },
+          { name: "Core: Pallof Press", reps: [10, 10, 10], sets: 3 },
+        ],
+      },
+      {
+        weekday: "Saturday",
+        date: "Saturday, April 19, 2025",
+        type: "Hot Yoga (Mobility & Recovery)",
+      },
+    ],
+  },
+  {
+    week: 2,
+    focus: "Progression & Speed Focus",
+    days: [
+      {
+        weekday: "Sunday",
+        date: "Sunday, April 20, 2025",
+        type: "Hot Yoga (Mobility & Recovery)",
+      },
+      {
+        weekday: "Monday",
+        date: "Monday, April 21, 2025",
+        type: "Short Strength (Unilateral Lower Body, 30-40 min)",
+        exercises: [
+          { name: "Lateral Lunges", reps: [10, 10, 10], sets: 3 },
+          { name: "Single Leg Pistol W Box ", reps: [10, 10, 10], sets: 3 },
+          { name: "Hip Thrust", reps: [10, 10, 10], sets: 3 },
+          { name: "Core: Reverse Crunches", reps: [10, 10, 10], sets: 3 },
+        ],
+      },
+      {
+        weekday: "Tuesday",
+        date: "Tuesday, April 22, 2025",
+        type: "Running (Speed or Tempo Focus, 20-30 min)",
+        workout: "30-sec sprint + 90-sec walk (repseat 6-8x)",
+      },
+      {
+        weekday: "Wednesday",
+        date: "Wednesday, April 23, 2025",
+        type: "Recovery Day",
+      },
+      {
+        weekday: "Thursday",
+        date: "Thursday, April 24, 2025",
+        type: "Short Upper Body Strength (30-40 min)",
+        exercises: [
+          {
+            name: "Single-Arm Dumbbell Bench Press",
+            reps: [10, 10, 10],
+            sets: 3,
+          },
+          { name: "Dumbbell Snatch", reps: [10, 10, 10], sets: 3 },
+          {
+            name: "Core: Side Plank with Hip Dips",
+            reps: [10, 10, 10],
+            sets: 3,
+          },
+        ],
+      },
+      {
+        weekday: "Friday",
+        date: "Friday, April 25, 2025",
+        type: "Jungle TO",
+      },
+      {
+        weekday: "Saturday",
+        date: "Saturday, April 26, 2025",
+        type: "Recovery Day",
+      },
+    ],
+  },
 ];
