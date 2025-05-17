db55c97364743e41f02e8e45b90425ec3d9050ec
atina <atinachang@gmail.com>
2025-05-17 19:56:50 -0400

    nav

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,53 +1,54 @@
 "use client";
 
-import { Box, Link } from "@mui/material";
+import { AppBar, Box, Link, Tab, Tabs, Toolbar, Typography } from "@mui/material";
 import { signIn, signOut } from "next-auth/react";
 
 import React from "react";
 import { useSession } from "next-auth/react";
 
 const Nav = () => {
-	const { status } = useSession();
+  const { status } = useSession();
 
-	if (status === "unauthenticated") {
-		return (
-			<Box
-				sx={{
-					display: "flex",
-					alignItems: "center",
-					textAlign: "center",
-					justifyContent: "space-around",
-					py: 3,
-				}}>
-				<Link href="/">Home</Link>
-				<Link component={"button"} onClick={() => signIn()}>
-					Login
-				</Link>
-			</Box>
-		);
-	}
+  if (status === "unauthenticated") {
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          textAlign: "center",
+          justifyContent: "space-around",
+          py: 3,
+        }}
+      >
+        <Link href="/">Home</Link>
+        <Link component={"button"} onClick={() => signIn()}>
+          Login
+        </Link>
+      </Box>
+    );
+  }
 
-	return (
-		<Box
-			sx={{
-				display: "flex",
-				alignItems: "center",
-				textAlign: "center",
-				justifyContent: "space-around",
-				py: 3,
-			}}>
-			<Link href="/">Home</Link>
-			<Link href="/progress">Progress</Link>
-			<Link
-				component={"button"}
-				onClick={() => {
-					signOut();
-				}}>
-				Log Out
-			</Link>
-			<Link href="/dashboard">Dashboard</Link>
-		</Box>
-	);
+  return (
+    <AppBar position="static" color="default" elevation={1}>
+      <Toolbar>
+        <Typography variant="h6" sx={{ flexGrow: 1, fontWeight: 600, color: "#FF6B6B" }}>
+          <Link href="/">Sweat Set</Link>
+        </Typography>
+
+        <Tabs value={0} textColor="inherit">
+          <Tab label="Progress" component={Link} href="/progress" />
+          <Tab label="Dashboard" component={Link} href="/dashboard" />
+          <Tab
+            label="Log Out"
+            component={Link}
+            onClick={() => {
+              signOut();
+            }}
+          />
+        </Tabs>
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default Nav;
