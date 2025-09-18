d7231e7b8db86ad99c3e969091ac82fa0aabd480
atina <atinachang@gmail.com>
2025-09-18 17:52:50 -0400

    cleanup

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,30 +1,31 @@
 import { DefaultSession } from "next-auth";
 
 declare module "next-auth" {
-	interface Session {
-		user: {
-			name: string;
-			userId: string;
-			firstName?: string;
-			// lastName?: string;
-			id: string;
-			email: string;
-		} & DefaultSession["user"];
-	}
+  interface Session {
+    user: {
+      name: string;
+      userId: string;
+      firstName?: string;
+      // lastName?: string;
+      id: string;
+      email: string;
+    } & DefaultSession["user"];
+  }
 
-	interface User extends DefaultUser {
-		name?: string;
-		userId?: string;
-		firstName?: string;
-		// lastName?: string;
-		email?: string;
-		id?: string;
-	}
+  interface User extends DefaultUser {
+    _id: string;
+    name?: string;
+    userId?: string;
+    firstName?: string;
+    // lastName?: string;
+    email?: string;
+    id?: string;
+  }
 }
 
 declare module "next-auth/jwt" {
-	interface JWT {
-		id: string;
-		role: number;
-	}
+  interface JWT {
+    id: string;
+    role: number;
+  }
 }
