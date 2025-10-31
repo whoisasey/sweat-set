932739e867c0a46aadf217627eadadb075822013
atina <atinachang@gmail.com>
2025-10-30 20:49:30 -0400

    width

--- app/components/ui/SetInputs.tsx
+++ app/components/ui/SetInputs.tsx
@@ -72,7 +72,7 @@ export default function SetInputs({ sets, reps, weights, onChange }: SetInputsPr
                   pattern: "[0-9]*",
                   style: { textAlign: "center" },
                 }}
-                sx={{ minWidth: 60 }}
+                sx={{ minWidth: 50 }}
               />
               <IconButton size="small" onClick={() => handleAdjust(i, "weight", 5)} aria-label="increase weight">
                 <Add fontSize="small" />
