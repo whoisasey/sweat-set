4ff5cb6358390b2cdfdbcd80e30e54c984dbb56d
atina <atinachang@gmail.com>
2025-11-13 20:40:12 -0500

    test

--- app/components/ui/SetInputs.tsx
+++ app/components/ui/SetInputs.tsx
@@ -73,13 +73,11 @@ export default function SetInputs({ sets, reps, weights, onChange }: SetInputsPr
                   }
                 }}
                 onBlur={(e) => {
-                  const parent = e.target.closest(".MuiPaper-root");
-                  if (parent) {
-                    parent.scrollTo({
-                      top: e.target.offsetTop - 50, // small offset
-                      behavior: "smooth",
-                    });
-                  }
+                  e.target.scrollIntoView({
+                    behavior: "smooth",
+                    block: "center",
+                    inline: "nearest",
+                  });
                 }}
                 size="small"
                 inputProps={{
