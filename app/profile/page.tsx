9071dd4bfcebc78afe61270157f7e6d2e89a5b60
atina <atinachang@gmail.com>
2025-11-19 19:56:00 -0500

    profile page template

---
+++ app/profile/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div>
+      {" "}
+      {/* User Profile */}
+      <li> Name: Atina Chang</li>
+      <li> Age: 33</li>
+      <li> Weight: 142lb</li>
+      <li></li>
+      <li></li>
+      {/* Goals */}
+      <li>Goals: </li>
+    </div>
+  );
+};
+
+export default ProfilePage;
